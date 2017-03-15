95dbe715c2f2889f6e6afc78089f0695ea97ec5c
Joakim Lönnegren <K403379@internal.financecorp.biz>
2017-03-15 08:35:15 +0100

    Initial commit

---
+++ OomNUnitExample/DbUtil.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OomNUnitExample
+{
+    public class DbUtil
+    {
+        public static void InsertSeed(string DbName)
+        {
+
+        }
+
+        public static void CreateDb(string dbName)
+        {
+            using (SqlConnection conn = new SqlConnection("Server=localhost;Database=Test01;Trusted_Connection=True;"))
+            {
+                var dropIfExists = string.Format("IF(SELECT COUNT(*) FROM sys.databases WHERE name = '{0}') > 0 BEGIN ALTER DATABASE [{0}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE DROP DATABASE [{0}] END", dbName);
+                using (SqlCommand cmd = new SqlCommand(dropIfExists, conn))
+                {
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+
+                }
+            }
+        }
+
+        public static void DropDb(string dbName)
+        {
+            using (SqlConnection conn = new SqlConnection("Server=localhost;Database=Test01;Trusted_Connection=True;"))
+            {
+                var dropIfExists = string.Format("IF(SELECT COUNT(*) FROM sys.databases WHERE name = '{0}') > 0 BEGIN ALTER DATABASE [{0}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE DROP DATABASE [{0}] END", dbName);
+                using (SqlCommand cmd = new SqlCommand(dropIfExists, conn))
+                {
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+
+                }
+            }
+        }
+
+    }
+}
---
+++ OomNUnitExample/Tests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OomNUnitExample
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private static object[] testCases;
+
+        public Tests()
+        {
+            testCases = new object[] { 1,2,3 };
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            DbUtil.CreateDb("Test01");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DbUtil.DropDb("Test01");
+        }
+
+        [Test]
+        [TestCaseSource(nameof(testCases))]
+        public void theTest()
+        {
+            Console.WriteLine("I ran");
+        }
+        
+    }
+}
